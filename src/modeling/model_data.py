91d213c166f62cef2cc1a8c8dbb6bc9c10f4fec2
Aureole Rogelio Edjekpoto <edjekpotorogelio7@gmail.com>
2026-04-04 03:37:07 +0100

    feat: implement star schema and data modeling

---
+++ src/modeling/model_data.py
@@ -0,0 +1,57 @@
+from src.utils.spark_session import create_spark_session
+from src.utils.logger import setup_logger
+from src.utils.config import CURATED_DATA_PATH, DIM_LOCATION_PATH, DIM_PROPERTY_PATH, DIM_DATE_PATH, FACT_SALES_PATH
+from pyspark.sql import functions as F
+from pyspark.sql import Window
+
+def create_star_schema():
+    logger = setup_logger()
+    spark = create_spark_session()
+    
+    logger.info("Starting dimension creation...")
+    df_curated = spark.read.parquet(CURATED_DATA_PATH)
+
+    # --- secure joins for null values ---
+    fill_values = {
+        "city": "UNKNOWN", "state": "UNKNOWN", "zip_code": 0,
+        "status": "UNKNOWN", "bed": 0, "bath": 0, "acre_lot": 0.0,
+        "house_size": 0.0, "property_size_category": "UNKNOWN"
+    }
+    df_curated = df_curated.fillna(fill_values)
+
+    # Create dim_location
+    dim_location = df_curated.select("city", "state", "zip_code").distinct().withColumn("location_id", F.monotonically_increasing_id())
+    dim_location.write.mode("overwrite").parquet(DIM_LOCATION_PATH)
+    logger.info("Dimension 'dim_location' created and saved.")
+    dim_location.show(10, truncate=False)
+
+    # Create dim_property
+    dim_property = df_curated.select("status", "bed", "bath", "acre_lot","house_size", "property_size_category").distinct().withColumn("property_id", F.monotonically_increasing_id())
+    dim_property.write.mode("overwrite").parquet(DIM_PROPERTY_PATH)
+    logger.info("Dimension 'dim_property' created and saved.")
+    dim_property.show(10, truncate=False)
+
+    # Create dim_date 
+    dim_date = df_curated.select("sale_date","sale_year","sale_month","sale_quarter").distinct().withColumn("date_id", F.monotonically_increasing_id())
+    dim_date.write.mode("overwrite").parquet(DIM_DATE_PATH)
+    logger.info("Dimension 'dim_date' created and saved.")
+    dim_date.show(10, truncate=False)
+
+    # Create fact_sales
+    #first join with dim_location to get location_id in fact_sales
+    fact_sales = df_curated.join(dim_location, on=["city", "state", "zip_code"], how="left") 
+
+    #second join with dim_property
+    fact_sales = fact_sales.join(dim_property, on=["status", "bed", "bath", "acre_lot","house_size", "property_size_category"], how="left")
+
+    #third join with dim_date
+    fact_sales = fact_sales.join(dim_date, on=["sale_date"], how="left")
+
+    #select only required columns for fact_sales
+    fact_sales = fact_sales.select("location_id", "property_id", "date_id", "price", "price_per_sqft")
+    fact_sales.write.mode("overwrite").parquet(FACT_SALES_PATH)
+    logger.info("Fact table 'fact_sales' created and saved.")
+    fact_sales.show(10, truncate=False)
+
+if __name__ == "__main__":
+    create_star_schema()
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,11 @@
+import os
 RAW_DATA_PATH = "data/raw/realtor-data.zip.csv"
 CLEANED_DATA_PATH = "data/cleaned/cleaned_data.parquet"
 CURATED_DATA_PATH = "data/curated/curated_data.parquet"
+
+#new path for data modeling
+MODELING_PATH = os.path.join("data", "modeling")
+DIM_LOCATION_PATH = os.path.join(MODELING_PATH, "dim_location")
+DIM_PROPERTY_PATH = os.path.join(MODELING_PATH, "dim_property")
+DIM_DATE_PATH = os.path.join(MODELING_PATH, "dim_date")
+FACT_SALES_PATH = os.path.join(MODELING_PATH, "fact_sales")
